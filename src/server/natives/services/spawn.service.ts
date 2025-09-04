fb710c622e03f8dd5467e6d758d79e2cbf959cc9
t0ke <t0ke@bitzone.com>
2025-09-04 18:45:21 +0300

    Update spawn service abstract methods

--- src/server/natives/services/spawn.service.ts
+++ src/server/natives/services/spawn.service.ts
@@ -6,15 +6,10 @@ import { ServerTypes } from '../../core/types';
 export abstract class RPSpawnService<
   T extends ServerTypes = ServerTypes,
 > extends RPServerService<T> {
-  abstract addSpawnPoint(spawnData: SpawnPointData): Promise<string>;
-  abstract removeSpawnPoint(spawnId: string): Promise<boolean>;
-  abstract getSpawnPoint(spawnId: string): SpawnPoint | undefined;
-  abstract getAllSpawnPoints(): SpawnPoint[];
-  abstract getRandomSpawnPoint(): SpawnPoint | undefined;
+  abstract setSpawnPoint(spawnData: SpawnPointData): void;
+  abstract clearSpawnPoint(): void;
+  abstract getSpawnPoint(): SpawnPoint | undefined;
   abstract loadSpawnsFromConfig(config: SpawnConfig): void;
+  
   abstract spawnPlayer(playerId: string, options?: PlayerSpawnOptions): void;
-  abstract setAutoSpawn(enabled: boolean): void;
-  abstract setAutoSpawnCallback(playerId: string, callback: () => void): void;
-  abstract removeAutoSpawnCallback(playerId: string): void;
-  abstract forceRespawn(playerId: string): void;
 }
